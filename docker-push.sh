68fb4ab45a16ddf5575f048872f20ba52b957b02
smeyerhot <smeyerhot@gmail.com>
2019-04-25 01:27:00 -0700

    new build with elb configured

--- docker-push.sh
+++ docker-push.sh
@@ -7,7 +7,7 @@ then
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
     # new
-    export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-1466309556.us-west-1.elb.amazonaws.com"
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-702996408.us-east-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
   fi
