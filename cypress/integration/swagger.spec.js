ecd0b8db1f7c7db745be52d07cad45d5719b3aa2
smeyerhot <smeyerhot@gmail.com>
2019-04-28 01:49:49 -0700

    hardcoding

--- cypress/integration/swagger.spec.js
+++ cypress/integration/swagger.spec.js
@@ -6,7 +6,7 @@ describe('Swagger', () => {
       .get('a').contains('Swagger').click();
 
       cy.get('select > option').then((el) => {
-        expect((el).text()).to.contain(Cypress.env('LOAD_BALANCER_DNS_NAME'));
+        expect((el).text()).to.contain(Cypress.env('http://testdriven-staging-alb-1466309556.us-west-1.elb.amazonaws.com'));
       });
   });
 });
