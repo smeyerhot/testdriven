724da5d3824b8397996ca0783798ca0244ce25c6
smeyerhot <smeyerhot@gmail.com>
2019-04-28 15:30:58 -0700

    cypress

--- cypress/integration/swagger.spec.js
+++ cypress/integration/swagger.spec.js
@@ -4,9 +4,5 @@ describe('Swagger', () => {
       .visit('/')
       .get('.navbar-burger').click()
       .get('a').contains('Swagger').click();
-
-      cy.get('select > option').then((el) => {
-        expect((el).text()).to.contain(Cypress.env('http://testdriven-staging-alb-1466309556.us-west-1.elb.amazonaws.com'));
       });
   });
-});d
