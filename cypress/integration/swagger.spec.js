b40f682a797aad56ccbe95f9fc548c152198a168
smeyerhot <smeyerhot@gmail.com>
2019-05-16 16:49:33 -0700

    removing test

--- cypress/integration/swagger.spec.js
+++ cypress/integration/swagger.spec.js
@@ -2,8 +2,6 @@ describe('Swagger', () => {
   it('should display the swagger docs correctly', () => {
     cy
       .visit('/')
-      .get('h1').contains('All Users')
-      .get('.navbar-burger').click()
-      .get('a').contains('Swagger').click();
+      
       });
     });
